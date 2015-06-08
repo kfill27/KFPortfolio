f6565cbf8444e5a72c156a728855aa3467d4bd0a
Kyra Fillmore <kfillmore27@gmail.com>
2015-06-08 16:35:38 -0400

    add pages for portfolio items- WynkedIn and Booksie

--- server.rb
+++ server.rb
@@ -3,3 +3,11 @@ require 'sinatra'
 get '/' do
 	File.read(File.join('public', 'index.html'))
 end
+
+get '/booksie' do
+  File.read(File.join('public', 'booksie.html'))
+end
+
+get '/wynkedin' do
+  File.read(File.join('public', 'wynkedin.html'))
+end
